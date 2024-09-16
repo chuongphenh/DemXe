3b7d23b8bd848278974512ea9368391297d7722e
Phạm Hữu Chương <89389078+chuongphenh@users.noreply.github.com>
2024-09-16 15:12:29 +0700

    feat: code demo DemXe

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DemXe
+{
+    internal class Program
+    {
+        // đọc dữ liệu từ file và trả về mảng các dòng
+        private static string[] ReadFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            return lines;
+        }
+        // đếm số lần xuất hiện của biển số trong mảng và trả về một từ điển
+        private static Dictionary<string, int> CountOccurrences(string[] lines)
+        {
+            var occurrences = new Dictionary<string, int>(StringComparer.Ordinal);
+
+            foreach (var line in lines)
+            {
+                if (occurrences.ContainsKey(line))
+                {
+                    occurrences[line]++;
+                }
+                else
+                {
+                    occurrences[line] = 1;
+                }
+            }
+
+            return occurrences;
+        }
+        // ghi số lần xuất hiện của biển số vào file
+        private static void WriteOccurrencesToFile(Dictionary<string, int> occurrences, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            {
+                foreach (var pair in occurrences)
+                {
+                    writer.WriteLine($"{pair.Key}: {pair.Value}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const string filename = "In.txt";
+            var lines = ReadFile(filename);
+            var occurrences = CountOccurrences(lines);
+            WriteOccurrencesToFile(occurrences, "Out.txt");
+        }
+    }
+}
